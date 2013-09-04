8f37a26e2759dd7083d653346e80b9d1633aaf67
Thomas Levine <occurrence@thomaslevine.com>
2013-09-04 17:58:48 -0400

    set the profile directory

--- crawl.py
+++ crawl.py
@@ -9,22 +9,12 @@ def randomsleep():
     seconds = min(max(2, seconds), 60)
     sleep(seconds)
 
-def driver_setup():
-    desired_capabilities = webdriver.DesiredCapabilities.CHROME
-    desired_capabilities['name'] = 'Chrome database sprint data collection'
-    driver = webdriver.Remote(
-        desired_capabilities=desired_capabilities,
-        command_executor="http://localhost:4444/wd/hub",
-        profile = webdriver.FirefoxProfile(profile_directory = 'George')
-    )
-    return driver
-
 def visible(a):
     return a.is_displayed() and a.is_enabled()
 
 def main():
     SEED = 'http://blog.zipfianacademy.com/post/46864003608/a-practical-intro-to-data-science'
-    driver = driver_setup()
+    driver = webdriver.Firefox(webdriver.FirefoxProfile(profile_directory = 'George'))
     driver.get(SEED)
     while True:
         anchors = filter(visible, driver.find_elements_by_xpath('//a'))
